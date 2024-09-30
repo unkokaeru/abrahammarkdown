bec4e5bd480488f40a4a55d6053d11d6d6ae90dd
unkokaeru <william@fayers.com>
2024-09-30 08:46:21 +0100

    docs: fix line length in doc strings

--- source/abrahammarkdown/__init__.py
+++ source/abrahammarkdown/__init__.py
@@ -1,3 +1,7 @@
-"""Converts University of Lincoln modules to folders of markdown, including notes, questions, and other module information. Designed for Obsidian."""
+"""
+Converts University of Lincoln modules to folders of markdown.
+
+Including notes, questions, and other module information. Designed for Obsidian.
+"""
 
 __version__ = "0.1.0"
